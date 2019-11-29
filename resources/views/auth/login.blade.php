6e851284162d5ac8f1a4d648daa126375ed66cec
fikfik <muhammadfikrimakmur@gmail.com>
2019-11-29 08:21:44 +0700

    Integrasi Google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,6 +31,13 @@
                                    placeholder="Password" name="password">
                         </div>
                         <input type="submit" class="btn btn-primary btn-lg btn-block" value="Login">
+                        <center>
+                        <br>
+                        or Login with
+                        <br>
+                        <br>
+                        <div class="g-signin2" data-onsuccess="onSignIn" data-onfailure="onFailure"></div>
+                        </center>
                     </form>
                     <div class="modal-footer bg-transparent "><p class="text-sm">Belum punya akun? </p><a
                             href="{{ route('register') }}"><p class="text-sm">Daftar Sekarang</p></a></div>
@@ -38,4 +45,23 @@
             </div>
         </div>
     </div>
+
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="449743998264-9hkpnp4hooi80ttq3ngmjo84bo5h2u9v.apps.googleusercontent.com">
+<script>
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  alert("SELAMAT DATANG "+profile.getName()+"!");
+}
+function onFailure(error) {
+    alert("Gagal login dengan Google");
+}
+</script>
 @endsection
+
